e7625f952255e412793d88c335ce2733869447a1
charlotte <charlotte.sgnz@gmail.com>
2019-11-04 21:46:26 +0100

    test

---
+++ src/components/home-page/Recherche.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="search">
+        <header>
+            <h1>Rechercher</h1>
+        </header>
+        <div class="search-container">
+            <form  @submit.prevent="search()">
+                <div class="form-row">
+                    <input class="search-input form-control" 
+                        type="text" 
+                        v-model="request" 
+                        placeholder="petite loupe"/>
+                    <button type="submit" 
+                        class="search-btn btn btn-primary">
+                        Valider
+                    </button>  
+                    <button type="" 
+                        class="filter-btn btn btn-primary">
+                        Filtrer
+                    </button>
+                </div>
+                <label> Localisation</label>
+                <input class="search-place form-control" 
+                        type="text" 
+                        v-model="place" />
+            </form>
+        </div>
+        <h3 v-show="show===true">Résultat trouvé pour {{resultat}}</h3>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                request: '',
+                resultat:'',
+                show: false
+            }
+        },
+        methods: {
+            search() {
+                this.show = true;
+                this.resultat = this.request
+            }
+        }
+    }
+</script>
+
+<style>
+
+.form-row {
+    margin-bottom: 10px;
+}
+
+.search-input {
+    width: 25%;
+    margin-right: 10px;
+}
+
+.filter-btn {
+    margin-left: 10px;
+}
+
+.search-place {
+    width: 10%;
+}
+
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,114 @@
 <template>
-    <div class="home">
-        <h1>Home page</h1>
-    </div>
+    <main class="root">
+        <aside class="menu">
+            <nav class="nav">
+                <ul>
+                    <li>
+                        <span><icon name="home" scale='1.5'></icon></span><span><a href='#' class="itMenu" @click="choice='accueil'">Accueil</a></span>
+                        <div v-show="choice==='accueil'" class="underline"></div>
+                    </li>
+                    <li>
+                        <icon name="search" scale='1.5'></icon><a href='#' class="itMenu" @click="choice='recherche'">Recherche</a>
+                        <div v-show="choice==='recherche'" class="underline"></div>
+                    </li>
+                    <li>
+                        <icon name="email" scale='1.5'></icon><a href='#' class="itMenu" @click="choice='messagerie'">Messagerie</a>
+                        <div v-show="choice==='messagerie'" class="underline"></div>
+                    </li>
+                    <li>
+                        <icon name="people" scale='1.5'></icon><a href='#' class="itMenu" @click="choice='abonnements'">Abonnements</a>
+                        <div v-show="choice==='abonnements'" class="underline"></div>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+        <div v-show="choice==='recherche'"><Recherche/></div>
+        <div v-show="choice==='messagerie'"><Messagerie/></div>
+        <div v-show="choice==='abonnements'"><Abonnements/></div>
+    </main>
+
+
+
 </template>
 
 <script>
+    import 'vue-md-icons/src/icons'
+    import Icon from 'vue-md-icons/src/components/Icon'
+    import Abonnements from '@/components/home-page/Abonnements'
+    import Messagerie from '@/components/home-page/Messagerie'
+    import Recherche from '@/components/home-page/Recherche'
     export default {
-        name: "home"
+        name: "home",
+        components: {
+            Icon,
+            Abonnements,
+            Messagerie,
+            Recherche
+        },
+        data() {
+            return {
+                choice: 'accueil'
+            }
+        }
     }
 </script>
 
 <style >
-
+    .menu {
+        position: fixed;
+        width: 200px;
+        height: 100vh;
+        background: #2D2D2D;
+    }
+    .nav {
+        position: relative;
+        text-align: right;
+        top: 20%;
+        transform: translateY(-50%);
+        font-weight: bold;
+    }
+    .nav ul {
+        list-style: none;
+        margin: -50px;
+        
+        
+    }
+    li {
+       position: relative;
+       margin: 1em 0; 
+       
+    }
+    .itMenu {
+        background: #2D2D2D;
+        border: none;
+        color:bisque;
+        opacity: 60%;
+        text-decoration: none;
+        text-decoration-color: none;
+    }
+    .itMenu:hover {
+        opacity: 100%;
+        text-decoration: none;
+        color: bisque;
+        
+    }
+    
+    .nav a:focus {
+        opacity: 100%;
+        text-decoration: none;
+        text-decoration-color: bisque;
+    }
+    .underline  {
+    width: 150px;
+    height: 2px;
+    background: linear-gradient(to right, #A04112, bisque);
+    margin-top: 10px;
+    margin-left: 48px;
+    
+    }
+    .md-icon {
+        color: white;
+        margin-top: -8px;
+        
+    }
 </style>
